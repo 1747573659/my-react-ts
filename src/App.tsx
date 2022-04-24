766114a0ec94be8c6cd21d76a128431d91d4c665
chensheng <chensheng@kemai.cn>
2022-04-24 13:44:48 +0800

    20220424_提交

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import { Button } from 'antd';
-import './App.css';
+import Login from './components/Login/Login'
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Button type="primary">Button</Button>
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
@@ -20,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        <Login></Login>
       </header>
     </div>
   );
---
+++ src/components/Login/Login.tsx
@@ -0,0 +1,7 @@
+import LoginForm from '../LoginForm/LoginForm'
+
+const Login = () => (
+  <LoginForm/>
+)
+
+export default  Login
